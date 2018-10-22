dc2278ab0ce91f01629fb4e7ac00976a9bb28ce7
Unknown <Josrodjr@gmail.com>
2018-10-22 13:04:42 -0600

    Display working

--- chismesapi/chismesapi/urls.py
+++ chismesapi/chismesapi/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('chismes.urls')),
     path('', include('frontapp.urls')),
 ]
